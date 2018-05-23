39550f8299c50be700ca2724c2df28f2021874da
rmk9 <rmk.9@yandex.ru>
2018-05-23 18:31:45 +0700

    [dev] Registration, authorization

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const flash = require('connect-flash');
+const passport = require('passport');
 const session = require('express-session');
 
 const app = express();
@@ -12,6 +13,8 @@ const app = express();
 //routes
 const notes = require('./routes/notes');
 const users = require('./routes/users');
+
+require('./config/passport')(passport);
 
 mongoose.Promise = global.Promise;
 
@@ -32,20 +35,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 
 app.use(session({
-    secret: 'secret',
-    resave: true,
-    saveUninitialized: true
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
 }));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(flash());
 
-app.use((req, res, next) => {
-    res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_msg = req.flash('error_msg');
-    res.locals.error = req.flash('error');
-    next();
+app.use(function(req, res, next){
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
 });
-
 app.get('/', (req, res) => {
     res.render('index');
 });
@@ -57,7 +63,7 @@ app.get('/about', (req, res) => {
 app.use('/notes', notes)
 app.use('/users', users)
 
-const port = 9310;
+const port = 9311;
 
 app.listen(port, () => {
     console.log(`server on ${port}`);
